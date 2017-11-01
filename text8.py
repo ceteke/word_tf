86877472afc72c2d9c5fc132b96c483b1ab9b223
Cem Eteke <ceteke13@ku.edu.tr>
2017-11-02 02:02:21 +0300

    logging improvements

--- text8.py
+++ text8.py
@@ -6,9 +6,10 @@ with open('text8', 'r', encoding='utf-8') as f:
 
 glove = pickle.load(open('glove.pkl', 'rb'))
 #glove = Glove(verbose=1)
-print("Fitting corpus...")
+#print("Fitting corpus...")
 #glove.fit_corpus(corpus, 10, 20000)
 #glove.save()
+print("Training")
 glove.train(100, 0.05, 25, 0.75, 100)
 #print(glove.most_similar(['were', 'are'], ['was'], topn=50))
 
