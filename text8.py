155fcf1ee83c33431543ec656632c8d9c00f1c59
Cem Eteke <ceteke13@ku.edu.tr>
2017-11-02 01:14:07 +0300

    verbose improve

--- text8.py
+++ text8.py
@@ -4,11 +4,11 @@ import pickle
 with open('text8', 'r', encoding='utf-8') as f:
     corpus = f.readlines()
 
-#glove = pickle.load(open('glove.pkl', 'rb'))
-glove = Glove(verbose=1)
+glove = pickle.load(open('glove.pkl', 'rb'))
+#glove = Glove(verbose=1)
 print("Fitting corpus...")
-glove.fit_corpus(corpus, 10, 20000)
-glove.save()
+#glove.fit_corpus(corpus, 10, 20000)
+#glove.save()
 glove.train(100, 0.05, 25, 0.75, 100)
 #print(glove.most_similar(['were', 'are'], ['was'], topn=50))
 
